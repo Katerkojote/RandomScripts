88a5f08538cbc257b85ed65f7cabad09369b402f
Kriss <64250674+Katerkojote@users.noreply.github.com>
2022-12-05 10:08:12 +0100

    changed the drive letter of backup drive and expanded the documentation

--- Backup.bat
+++ Backup.bat
@@ -1,9 +1,9 @@
-Rem Holt sich das aktuelle Datum und formuliert das in ein Brauchbares Format
+Rem Gets the current date and shortens it to override backups monthly
 set datenr=%date:~0,-8%
 Rem Creates new folder on Backup Drive
 mkdir H:\Desktop_Backup\%datenr%
 Rem Puts your choosen directorys into the Backup Drive
-xcopy C:\Users\%USERNAME%\Desktop\ H:\quickBackup\%datenr%\Desktop\ /R /E /Y
-xcopy C:\Users\%USERNAME%\%HOMEPATH%\Pictures\ H:\quickBackup\%datenr%\Pictures\ /R /E /Y
-xcopy C:\Users\%USERNAME%\%HOMEPATH%\Documents\ H:\quickBackup\%datenr%\Documents\ /R /E /Y
-Rem Geschrieben von Kris O. am 02.12.2022
+Rem Dont Forget to Replace [DriveLetter] with your actual Driveletter
+xcopy C:\Users\%USERNAME%\Desktop\ [DriveLetter]\quickBackup\%datenr%\Desktop\ /R /E /Y
+xcopy C:\Users\%USERNAME%\Pictures\ [DriveLetter]\quickBackup\%datenr%\Pictures\ /R /E /Y
+xcopy C:\Users\%USERNAME%\Documents\ [DriveLetter]\quickBackup\%datenr%\Documents\ /R /E /Y
