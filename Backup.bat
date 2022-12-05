19527b212a614a31d1fa25e2de710d969b512fda
Kriss <64250674+Katerkojote@users.noreply.github.com>
2022-12-05 14:27:49 +0100

    Cleaned up some Issues

--- Backup.bat
+++ Backup.bat
@@ -1,7 +1,8 @@
+Rem run shell:startup in the run prompt and put the skript there
 Rem Gets the current date and shortens it to override backups monthly
 set datenr=%date:~0,-8%
 Rem Creates new folder on Backup Drive
-mkdir H:\Desktop_Backup\%datenr%
+mkdir H:\quickBackup\%datenr%
 Rem Puts your choosen directorys into the Backup Drive
 Rem Dont Forget to Replace [DriveLetter] with your actual Driveletter
 xcopy C:\Users\%USERNAME%\Desktop\ [DriveLetter]\quickBackup\%datenr%\Desktop\ /R /E /Y
